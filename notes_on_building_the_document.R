5f310b27175fd5392a86d85b94747f5c76f26cb4
Ian Taylor <ian.taylor@noaa.gov>
2022-12-02 06:47:02 -0800

    add R notes on building PDF, initial bridging

--- notes_on_building_the_document.R
+++ notes_on_building_the_document.R
@@ -1,4 +1,5 @@
-# drafting the initial document (only had to be run once)
+# drafting the initial document 
+# (only had to be run once, or after significant change to sa4ss package)
 remotes::install_github("pfmc-assessments/sa4ss")
 library(sa4ss)
 setwd("doc")
@@ -7,8 +8,35 @@ sa4ss::draft(authors = c("Ian G. Taylor", "Vladlena Gertseva"),
              species = "petrale sole",
              latin = "Eopsetta jordani",
 )
-
-# build the document after making changes
-setwd("doc")
-bookdown::render_book("00a.Rmd", clean = FALSE, output_dir = getwd())
 setwd("..")
+
+# specifying the base model
+setwd("doc") # assumes working directory is already in "petrale"
+base <- "../models/2021.001.001_new_endyr" # relative to "docs"
+# creating the standard r4ss plots
+mod_base <- SS_output(base)
+SS_plots(mod_base)
+
+# Create a model Rdata object and tex tables
+sa4ss::read_model(
+    mod_loc = base,
+	create_plots = FALSE, 
+    fecund_mult = 'mt',
+	save_loc = "tables"
+)
+
+# load the Rdata file for the base model created by sa4ss::read_model() 
+load("00mod.Rdata")
+
+# clean out old version of file that gets compiled into the PDF
+setwd("doc") # skip this if already in "doc"
+if(file.exists("_main.Rmd")){
+	file.remove("_main.Rmd")
+}
+# Render the pdf
+bookdown::render_book("00a.Rmd", clean=FALSE, output_dir = getwd())
+setwd("..")
+
+### OPTIONAL
+# Use to only render a specific section which can be quicker
+bookdown::preview_chapter("01executive.Rmd", preview = TRUE, clean = FALSE)
