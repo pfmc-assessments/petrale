711a635a86c9787a0b4e5caeb16df84139d42df1
Ian Taylor <ian.taylor@noaa.gov>
2023-03-30 15:14:55 -0700

    add weight-length methods to biology section

--- notes_on_building_the_document.R
+++ notes_on_building_the_document.R
@@ -1,7 +1,10 @@
 # drafting the initial document 
 # (only had to be run once, or after significant change to sa4ss package)
-remotes::install_github("pfmc-assessments/sa4ss")
+pak::pkg_install("pfmc-assessments/sa4ss")
+pak::pkg_install("pfmc-assessments/sa4ss@table_functions_78")
+
 library(sa4ss)
+library(magrittr)
 setwd("documents")
 sa4ss::draft(authors = c("Ian G. Taylor", "Vladlena Gertseva"), 
              create_dir = FALSE,
@@ -12,10 +15,10 @@ setwd("..")
 
 # specifying the base model
 setwd("documents") # assumes working directory is already in "petrale"
-base <- "../models/2023.005.001" # relative to "docs"
+base <- "../models/2023.001.001_new_endyr" # relative to "docs"
 # creating the standard r4ss plots
-mod_base <- SS_output(base)
-SS_plots(mod_base)
+mod_base <- SS_output(base, printstats = FALSE, verbose = FALSE)
+#SS_plots(mod_base)
 
 # Create a model Rdata object and tex tables
 sa4ss::read_model(
@@ -27,6 +30,7 @@ sa4ss::read_model(
 
 # load the Rdata file for the base model created by sa4ss::read_model() 
 load("00mod.Rdata")
+mod_base <- model
 
 # clean out old version of file that gets compiled into the PDF
 setwd("documents") # skip this if already in "documents"
@@ -39,4 +43,4 @@ setwd("..")
 
 ### OPTIONAL
 # Use to only render a specific section which can be quicker
-bookdown::preview_chapter("01executive.Rmd", preview = TRUE, clean = FALSE)
+bookdown::preview_chapter(input = "22biology.Rmd")
