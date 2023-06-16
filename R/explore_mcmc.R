6e6e27ab366e3b3c9432a5ac0ef196ed5b34b123
Ian Taylor <ian.taylor@noaa.gov>
2023-06-16 11:34:53 -0700

    refining adnuts script

--- R/explore_mcmc.R
+++ R/explore_mcmc.R
@@ -16,18 +16,29 @@
 # model to explore
 dir_model <- "models/2023.a024.001_min_sample"
 dir_model <- "models/2023.a024.010_recdev2_h0.8"
-dir_adnuts <- file.path(dir_model, "adnuts")
-dir_adnuts <- file.path(dir_model, "adnuts2e6")
+dir_model <- "models/2019.001.001_base"
+dir_model <- "models/2023.a024.018_min_sample_retuned"
+dir_model <- "models/2023.a024.021_min_sample_retuned_recdev2_h0.8"
+dir_adnuts <- file.path(dir_model, "adnuts_")
+#dir_adnuts <- file.path(dir_model, "adnuts2e6")
 # copy everything to a folder within the model directory
+dir.create(dir_adnuts)
 R.utils::copyDirectory(dir_model, dir_adnuts, recursive = FALSE)
+# re-run to get covar
+tictoc::tic()
+run(dir_adnuts, skipfinished = FALSE)
+# re-run with mcmc to revise stuff (not sure if this makes a difference)
+run(dir_adnuts, extras = "-mcmc 100 -hbf", skipfinished = FALSE)
+tictoc::toc()
 tictoc::tic()
 fit <- adnuts::sample_rwm(model = 'ss', # this is the name of the executable
                           path = dir_adnuts, # directory with executable, input file, MLE output files (including covariance)
-                          iter = 2e6, # 200000,
+                          iter = 3e5, # 2e5, # 2e6, # 200000,
                           thin = 100, # thin to save memory, could try not
                           chains = 3)
 # By default the first 50% is burn in.
 tictoc::toc()
+save(fit, file = file.path(dir_adnuts, "fit_3e5_11June.Rdata"))
 
 # get parameter names
 model <- r4ss::SS_output(dir_model, printstats = FALSE, verbose = FALSE)
@@ -45,6 +56,7 @@ report_info <-
 
 # get informative parameter labels used by Stock Synthesis
 # model <- r4ss::SS_output() # read model output
+model <- r4ss::SS_output(dir_model, printstats = FALSE, verbose = FALSE)
 est_par_labels <- model$parameters %>% 
   dplyr::filter(!is.na(Active_Cnt)) %>%
   dplyr::pull(Label)
