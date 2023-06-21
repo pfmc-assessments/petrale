f73715ea515ad55902d4d68751ca211d3b1265c9
Ian Taylor <ian.taylor@noaa.gov>
2023-06-21 13:18:16 -0700

    update qqplots
    - see https://github.com/kellijohnson-NOAA/indexwc/issues/11

---
+++ Rscripts/plot_qqplots.R
@@ -0,0 +1,19 @@
+# Update Q-Q plots after changes to {indexwc}
+# https://github.com/kellijohnson-NOAA/indexwc/issues/11
+
+# update {indexwc} after change to QQ plot function
+pak::pak("kellijohnson-NOAA/indexwc")
+
+# load output from petrale index from WCGBTS
+dir_index <- "data-raw/indices/wcgbts/delta_lognormal/index/"
+load(file.path(dir_index, "sdmTMB_save.RData"))
+
+# make new QQ plots
+indexwc::plot_qq(fit, file.path(dir_index, "qq_new.png"))
+
+# load output from petrale index from triennial
+dir_index <- "data-raw/indices/triennial/delta_lognormal_no_depth/index/"
+load(file.path(dir_index, "sdmTMB_save.RData"))
+
+# make new QQ plots
+indexwc::plot_qq(fit, file.path(dir_index, "qq_new.png"))
