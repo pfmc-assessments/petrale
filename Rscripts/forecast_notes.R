d9b3dc1ac658bb838d8eb16497c68f7741a5cf28
Ian Taylor <ian.taylor@noaa.gov>
2023-07-21 13:53:33 -0700

    add placeholder decision table
    - relates to https://github.com/pfmc-assessments/sa4ss/issues/78

---
+++ Rscripts/forecast_notes.R
@@ -0,0 +1,46 @@
+if (FALSE) {
+  SSexecutivesummary(mod.34.10, forecast_ofl = c(3763, 3563))
+  save_loc <- file.path(mod.34.10$inputs$dir, "tex_tables")
+  dir.create(save_loc)
+  sa4ss::es_table_tex(
+    dir = mod.34.10$inputs$dir,
+    save_loc = save_loc
+  )
+
+  sa4ss::es_table_tex(
+    # dir = mod.34.10$inputs$dir,
+    dir = "models/2023.a034.010_fixed_forecast_catch/tables/",
+    csv_name = "table_label_projection_modified.csv",
+    digits = 0,
+    save_loc = save_loc
+  )
+
+
+
+  # Row-group names for first column; kableExtra::linebreak allows for hard returns
+  rowgroupnames <- kableExtra::linebreak(
+    x = c(
+      # "Recent\n avg.\n catch",
+      # "ACL\n$P^*$=0.40",
+      "ACL $P^*$=0.45"
+    ),
+    align = "l"
+  )
+} # end if (FALSE) to help with sourcing the commands below
+
+
+### make decision table
+#devtools::load_all();
+caption <- "Decision table with 10-year projections. 'Mgmt' refers to the three management scenarios (A) the default harvest control rule $P^* = 0.45$, (B) [TODO: fill in something here], and (C) [TODO: fill in something here]. In each case the 2023 and 2024 catches are fixed at the ACLs which have been set for that year with estimated fleet allocation provided  by the GMT. The alternative states of nature ('Low', 'Base', and 'High') are provided in the columns, with Spawning Output ('Spawn', in trillions of eggs) and Fraction of unfished ('Frac') provided for each state. The colors of catch and fraction unfished are relative with lighter colors representing lower values."
+
+tab <- table_decision(
+  caption = caption,
+  label = "es-decision",
+  list(mod.34.10, mod.34.10, mod.34.10),
+  list(mod.34.10, mod.34.10, mod.34.10),
+  list(mod.34.10, mod.34.10, mod.34.10)
+)
+writeLines(tab, "documents/tex_tables/decision_table.tex")
+
+# kableExtra::save_kable(file = "documents/tex_tables/decision_table.tex")
+
