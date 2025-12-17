1518e7bf67696f5941146ab21e88fe8e7db3b841
iantaylor-NOAA <ian.taylor@noaa.gov>
2025-12-17 12:51:27 -0800

    update U&A script

--- Rscripts/process_indices_2024_new_boundary.R
+++ Rscripts/process_indices_2024_new_boundary.R
@@ -1,4 +1,8 @@
 # first load indexwc with boundaries argument added to run_sdmbtmb()
+# this is currently in the "tribal_UandA" branch
+# https://github.com/pfmc-assessments/indexwc/compare/main...tribal_UandA
+
+library(ggplot2)
 indexwc_dir <- "c:/github/indexwc"
 devtools::load_all(indexwc_dir)
 
@@ -9,7 +13,7 @@ configuration <- tibble::as_tibble(read.csv(
 # subset to just WCGBTS petrale row of configuration file
 configuration <- configuration %>%
   dplyr::filter(species == "petrale sole" & source == "NWFSC.Combo")
-  
+
 str(configuration)
 # tibble [1 x 15] (S3: tbl_df/tbl/data.frame)
 #  $ species        : chr "petrale sole"
@@ -34,66 +38,95 @@ data <- configuration %>%
   # Pull the data based on the function found in fxn column
   dplyr::mutate(
     data_raw = list(format_data(eval(parse(text = fxn)))),
-    data_filtered = list(data_raw %>%
-      dplyr::filter(
-        depth <= min_depth, depth >= max_depth,
-        latitude >= min_latitude, latitude <= max_latitude,
-        year >= min_year, year <= max_year
-      ))
+    data_filtered = list(
+      data_raw %>%
+        dplyr::filter(
+          depth <= min_depth,
+          depth >= max_depth,
+          latitude >= min_latitude,
+          latitude <= max_latitude,
+          year >= min_year,
+          year <= max_year
+        )
+    )
   ) %>%
   dplyr::ungroup()
 
 # command below created by taking pieces of configuration above
 run_sdmtmb(
-    data = data$data_filtered[[1]],
-    formula = "catch_weight ~ 0 + fyear + pass_scaled",
-    family = sdmTMB::delta_lognormal(),
-    anisotropy = TRUE,
-    n_knots = 500,
-    boundaries = boundaries_data[c("Coastwide", "North of 46deg 53.3min", "South of 46deg 53.3min")],
-    spatiotemporal = list("iid", "iid")
+  data = data$data_filtered[[1]],
+  formula = "catch_weight ~ 0 + fyear + pass_scaled",
+  family = sdmTMB::delta_lognormal(),
+  anisotropy = TRUE,
+  n_knots = 500,
+  boundaries = boundaries_data[c(
+    "Coastwide",
+    "North of 46deg 53.3min",
+    "South of 46deg 53.3min"
+  )],
+  spatiotemporal = list("iid", "iid")
 )
 
-# manually copy results from 
-# "petrale_sole/wcgbts/delta_lognormal"
-# to 
-# "data-raw/wcgbts/delta_lognormal_with2023_and_UandA_boundary"
+# copy all files from temporary directory created by indexwc to a permanent directory
+from_dir <- "petrale_sole/wcgbts/delta_lognormal"
+to_dir <- "data-raw/wcgbts/delta_lognormal_with2024_and_UandA_boundary"
 
-est_by_area <- read.csv("data-raw/wcgbts/delta_lognormal_with2023_and_UandA_boundary/index/est_by_area.csv")
-results_coast <- est_by_area |> 
-  dplyr::filter(area == "Coastwide") 
-results_north <- est_by_area |> 
-  dplyr::filter(area == "North of 46deg 53.3min") |> 
+if (!dir.exists(to_dir)) {
+  dir.create(to_dir, recursive = TRUE)
+}
+files_to_copy <- list.files(from_dir, full.names = TRUE)
+file.copy(files_to_copy, to_dir, overwrite = TRUE, recursive = TRUE)
+
+
+est_by_area <- read.csv(
+  "data-raw/wcgbts/delta_lognormal_with2024_and_UandA_boundary/index/est_by_area.csv"
+)
+results_coast <- est_by_area |>
+  dplyr::filter(area == "Coastwide")
+results_north <- est_by_area |>
+  dplyr::filter(area == "North of 46deg 53.3min") |>
   dplyr::mutate(ratio = est / results_coast$est)
 
 
 (mean_all <- results_north$ratio |> mean() |> round(3))
-# [1] 0.157
+# [1] 0.159
 
-(mean_recent5 <- results_north |> 
-  dplyr::filter(year >= 2018) |> 
-  dplyr::pull(ratio) |> 
-  mean() |> 
+(mean_recent5 <- results_north |>
+  dplyr::filter(year >= 2018) |>
+  dplyr::pull(ratio) |>
+  mean() |>
   round(3))
-# [1] 0.18
+# [1] 0.183
 
-results_north |> 
-  ggplot(aes(x = year, y = ratio)) + 
-    geom_point() + 
-    geom_line(aes(y = mean_all, color = "mean of all obs"), lwd = 1) +
-    geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2, color = "mean of recent 5 obs"), 
-      lwd = 1,
-      data = data.frame(x1 = 2018, x2 = 2023, y1 = mean_recent5, y2 = mean_recent5)) +
-    # commented out regression
-    # stat_summary(fun.data=mean_cl_normal) + 
-    # geom_smooth(method='lm') + 
-    expand_limits(y = c(0, 0.25)) + 
-    scale_x_continuous(breaks = seq(2003, 2023, 2)) +
-    ylab(expression("Ratio of estimated biomass North of 46\u00B053.3' to coastwide biomass")) +
-    theme_bw()
+results_north |>
+  ggplot(aes(x = year, y = ratio)) +
+  geom_point() +
+  geom_line(aes(y = mean_all, color = "mean of all obs"), lwd = 1) +
+  geom_segment(
+    aes(x = x1, y = y1, xend = x2, yend = y2, color = "mean of recent 5 obs"),
+    lwd = 1,
+    data = data.frame(
+      x1 = 2018,
+      x2 = 2024,
+      y1 = mean_recent5,
+      y2 = mean_recent5
+    )
+  ) +
+  # commented out regression
+  # stat_summary(fun.data=mean_cl_normal) +
+  # geom_smooth(method='lm') +
+  expand_limits(y = c(0, 0.25)) +
+  scale_x_continuous(breaks = seq(2003, 2024, 2)) +
+  ylab(expression(
+    "Ratio of estimated biomass North of 46\u00B053.3' to coastwide biomass"
+  )) +
+  theme_bw()
 
-ggsave("data-raw/wcgbts/delta_lognormal_with2023_and_UandA_boundary/ratio_plot.png", 
-    width = 10, height = 7)
+ggsave(
+  "data-raw/wcgbts/delta_lognormal_with2024_and_UandA_boundary/ratio_plot.png",
+  width = 10,
+  height = 7
+)
 
 results_north |> dplyr::select(ratio, year) |> lm()
 # Call:
@@ -101,15 +134,80 @@ results_north |> dplyr::select(ratio, year) |> lm()
 
 # Coefficients:
 # (Intercept)         year
-#   -4.883590     0.002504
+#    -4.99565      0.00256
 results_north |> dplyr::pull(ratio) |> range() |> round(3)
-# [1] 0.127 0.217
+# [1] 0.127 0.215
 
-results_coast |> dplyr::select(year, est) |> 
-  round() |> 
-  dplyr::rename(Year = year, "Coastwide biomass (mt)" = est) |> 
-  dplyr::mutate("Biomass North of 46\u00B053.3' (mt)" = round(results_north$est)) |> 
-  dplyr::mutate(Ratio = round(results_north$ratio, 3)) |> 
-  knitr::kable(format = "html") |> 
-  writeLines("data-raw/wcgbts/delta_lognormal_with2023_and_UandA_boundary/results_table.html")
+results_coast |>
+  dplyr::select(year, est) |>
+  round() |>
+  dplyr::rename(Year = year, "Coastwide biomass (mt)" = est) |>
+  dplyr::mutate(
+    "Biomass North of 46\u00B053.3' (mt)" = round(results_north$est)
+  ) |>
+  dplyr::mutate(Ratio = round(results_north$ratio, 3)) |>
+  knitr::kable(format = "html") |>
+  writeLines(
+    "data-raw/wcgbts/delta_lognormal_with2024_and_UandA_boundary/results_table.html"
+  )
 
+avg_5yr_coast <- results_coast |>
+  dplyr::filter(year >= 2019) |>
+  dplyr::pull(est) |>
+  mean() |>
+  round()
+
+avg_5yr_north <- results_north |>
+  dplyr::filter(year >= 2019) |>
+  dplyr::pull(est) |>
+  mean() |>
+  round()
+
+avg_coast <- results_coast |>
+  dplyr::pull(est) |>
+  mean() |>
+  round()
+
+avg_north <- results_north |>
+  dplyr::pull(est) |>
+  mean() |>
+  round()
+
+data.frame(
+  "Average Coastwide Biomass (mt)" = c(avg_coast, avg_5yr_coast),
+  "Average Biomass North of 46\u00B053.3' (mt)" = c(avg_north, avg_5yr_north),
+  "Average Ratio" = c(
+    mean_all,
+    mean_recent5
+  ),
+  row.names = c("All years", "Recent 5 years")
+) |>
+  knitr::kable(format = "html") |>
+  writeLines(
+    "data-raw/wcgbts/delta_lognormal_with2024_and_UandA_boundary/averages_table.html"
+  )
+
+# remake index plot with fewer lines
+p <- est_by_area |>
+  dplyr::filter(
+    area %in% c("Coastwide", "North of 46deg 53.3min", "South of 46deg 53.3min")
+  ) |>
+  indexwc::plot_indices()
+
+p +
+  scale_x_continuous(breaks = seq(2003, 2024, 2)) +
+  ylab("Estimated biomass (mt)") +
+  ggplot2::scale_colour_manual(
+    values = c(
+      "Coastwide" = "black",
+      "North of 46deg 53.3min" = "blue",
+      "South of 46deg 53.3min" = "red"
+    )
+  ) +
+  expand_limits(y = 0) +
+  scale_y_continuous(expand = c(0, 0))
+ggsave(
+  "data-raw/wcgbts/delta_lognormal_with2024_and_UandA_boundary/index_modified.png",
+  width = 10,
+  height = 7
+)
